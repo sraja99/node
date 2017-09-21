0a9734f3e1a260e1a1e7b591ebb4f321fcfdfa3e
sraja99 <sraja@digital-lync.com>
2017-09-21 07:18:27 +0000

    &

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello start................. *********************************world");
+  res.send("Hello start&&&&&&&&&&&&&&&&................. *********************************world");
 });
 
 app.listen(5000);
